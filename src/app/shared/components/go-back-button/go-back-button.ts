d4693fd8089dd09d62afadf8f84e2a5ddba1ce18
bsy44 <syboubacar444@gmail.com>
2025-10-22 08:00:05 +0200

    feat(table-result): ajout d'un composant commun goBack button

---
+++ src/app/shared/components/go-back-button/go-back-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-go-back-button',
+  imports: [],
+  templateUrl: './go-back-button.html',
+  styleUrl: './go-back-button.css'
+})
+export class GoBackButton {
+
+}
