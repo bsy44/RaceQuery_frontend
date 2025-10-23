d8ee5141353b30365ec3adbfc372be666d0941e1
bsy44 <syboubacar444@gmail.com>
2025-10-23 22:51:41 +0200

    feat(page): ajout de page/ et de page notFound

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,17 @@ import { Teams } from './teams/teams';
 import { Races } from './races/races';
 import { RaceResult } from './race-result/race-result';
 import { RaceDetail } from './race-details/race-detail';
+import {NotFound} from './pages/not-found/not-found';
 
 export const routes: Routes = [
-  {path: 'dashboard', component: Dashboard},
+  {path: '', redirectTo:'home', pathMatch: 'full' },
+  {path: 'home', component: Dashboard},
   {path: 'driver/standings', component: Drivers},
   {path: 'team/standings', component: Teams},
   {path: 'races', component: Races},
   {path: 'race/:season/:gpName/results', component: RaceResult},
-  {path: 'race/:season/:gpName/details', component: RaceDetail}
+  {path: 'race/:season/:gpName/details', component: RaceDetail},
+  {path:'**', component: NotFound}
 ];
 
 @NgModule({
