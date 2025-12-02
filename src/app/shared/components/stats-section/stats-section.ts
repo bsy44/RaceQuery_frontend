1f14e464ec6df4894cdffee3383c53bbe084db14
bsy44 <syboubacar444@gmail.com>
2025-12-02 18:10:37 +0100

    feat(team-detail): ajout du composant stat, responsive de team stat et team detail

---
+++ src/app/shared/components/stats-section/stats-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stats-section',
+  imports: [],
+  templateUrl: './stats-section.html',
+  styleUrl: './stats-section.css',
+})
+export class StatsSection {
+
+}
--- src/app/teams/components/team-stats/team-stats.ts
+++ src/app/teams/components/team-stats/team-stats.ts
@@ -1,11 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { TeamDetailModel } from '../../models/teamDetail.model';
-import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-team-stats',
   imports: [
-    NgClass
   ],
   templateUrl: './team-stats.html',
   styleUrl: './team-stats.css',
