bd41a32d497e78328db65e16fc140221d2c4fd56
bsy44 <syboubacar444@gmail.com>
2025-12-18 00:23:42 +0100

    fix(top-standings): supression du type any

--- src/app/home/components/top-standings/top-standings.ts
+++ src/app/home/components/top-standings/top-standings.ts
@@ -1,28 +1,30 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import {NgClass} from '@angular/common';
-import {DriverStandingsModel} from '../../../standings/models/driverStanding.model';
-import {TeamStandingModel} from '../../../standings/models/teamStanding.model';
+import { NgClass } from '@angular/common';
+import { DriverStandingsModel } from '../../../standings/models/driverStanding.model';
+import { TeamStandingModel } from '../../../standings/models/teamStanding.model';
+
+type StandingItem = DriverStandingsModel | TeamStandingModel;
 
 @Component({
   selector: 'app-top-standings',
+  standalone: true,
   imports: [
     RouterLink,
     NgClass
   ],
   templateUrl: './top-standings.html',
-  styleUrl: './top-standings.css',
-  standalone: true
+  styleUrl: './top-standings.css'
 })
 export class TopStandings {
-  @Input() top5: (DriverStandingsModel | TeamStandingModel)[] = [];
+  @Input() top5: StandingItem[] = [];
   @Input() type: 'drivers' | 'teams' = 'drivers';
 
-  isDriver(item: any): item is DriverStandingsModel {
+  isDriver(item: StandingItem): item is DriverStandingsModel {
     return this.type === 'drivers';
   }
 
-  isTeam(item: any): item is TeamStandingModel {
+  isTeam(item: StandingItem): item is TeamStandingModel {
     return this.type === 'teams';
   }
 }
